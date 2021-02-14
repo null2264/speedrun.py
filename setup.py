b3660980369680872f6f1c586178f910a227fb67
ziro <palembani@gmail.com>
2021-02-14 17:20:55 +0700

    + Idk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 requirements = []
 with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
+    requirements = f.read().splitlines()
 
 readme=""
 with open('README.md') as f:
@@ -11,15 +11,17 @@ with open('README.md') as f:
 setup(
     name="speedrun.py",
     packages=["speedrunpy"],
-    version="0.0.8",
+    version="0.0.9",
     license="MIT",
-    description="Async speedrun.com API wrapper",
+    description="Async API Wrapper for speedrun.com",
     long_description=readme,
     long_description_content_type='text/markdown',
     author="null2264",
     author_email="palembani@gmail.com",
     url="https://github.com/null2264/speedrun.py",
-    install_requires=requirements,
+    install_requires=[
+        "aiohttp"
+    ],
     python_requires='>=3.8',
     classifiers=[
         'Development Status :: 3 - Alpha',
