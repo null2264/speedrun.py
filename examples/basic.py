2270d74ccd8044777a7fded53a51cd7416e659dc
ziro <palembani@gmail.com>
2021-08-06 15:41:29 +0700

    + Added basic example

---
+++ examples/basic.py
@@ -0,0 +1,11 @@
+import asyncio
+from speedrunpy import Client
+
+
+async def main():
+    src = Client()
+    games = await src.get_games(name="Super Mario Sunshine")
+    game = games.data[0]
+    print(game.id, game.name)
+
+asyncio.run(main())
